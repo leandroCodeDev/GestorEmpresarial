d9724f84fa1ba6001ebee75147b2f2d78af2a6de
Leandro Dias <lvd@certi.org.br>
2025-10-13 12:45:06 -0300

    test: removido mock de service

--- GestorEmpresarial-API/tests/Feature/EmpresaTest.php
+++ GestorEmpresarial-API/tests/Feature/EmpresaTest.php
@@ -5,6 +5,7 @@ use App\Http\Resources\EmpresaCollection;
 use App\Http\Resources\EmpresaResource;
 use App\Models\Empresa;
 use App\Services\EmpresaService;
+use Database\Seeders\EmpresaSeeder;
 use Illuminate\Testing\TestResponse;
 use Mockery;
 use function Pest\Laravel\getJson;
@@ -12,57 +13,38 @@ use function Pest\Laravel\postJson;
 use function Pest\Laravel\putJson;
 use function Pest\Laravel\deleteJson;
 
-beforeEach(function () {
-    // Cria um mock do service
-    $this->mockService = Mockery::mock(EmpresaService::class);
-    $this->app->instance(EmpresaService::class, $this->mockService);
-});
+
 
 afterEach(function () {
     Mockery::close();
 });
 
 it('retorna a lista de empresas', function () {
-    $empresas = Empresa::factory()->count(10)->make();
-    $empresaResource = new EmpresaCollection($empresas);
+    $empresas = Empresa::factory()->count(10)->create();
 
-
-    $this->mockService
-        ->shouldReceive('buscarTodos')
-        ->once()
-        ->andReturn($empresaResource);
-
-    $response = getJson('/api/empresas');
+    $response = $this->get('/api/empresas');
 
     $response->assertOk()
         ->assertJsonCount($empresas->count());
 });
 
 it('retorna uma empresa específica', function () {
-    $dados = Empresa::factory()->make();
-    $empresaResource = new EmpresaResource($dados);
-    $this->mockService
-        ->shouldReceive('buscarPorId')
-        ->with(10)
-        ->once()
-        ->andReturn($empresaResource);
+    $this->seed(EmpresaSeeder::class);
+    $empresa = Empresa::get()->first();
 
-    $response = $this->get('/api/empresas/10');
+    $response = $this->get('/api/empresas/'.$empresa->id);
+
 
     $response->assertOk()
-        ->assertJsonFragment($dados->toArray());
+        ->assertJsonFragment(['nome' => $empresa->nome])
+        ->assertJsonFragment(['cnpj' => $empresa->cnpj]);
 });
 
 it('cria uma nova empresa', function () {
     $dados = Empresa::factory()->make();
     $dados->nome = 'Nova Empresa';
+    $dados->endereco = 'novo endereço';
     $empresaCriada = $dados;
-    $empresaResource = new EmpresaResource($empresaCriada);
-
-    $this->mockService
-        ->shouldReceive('salvar')
-        ->once()
-        ->andReturn($empresaResource);
 
     $response = $this->post('/api/empresas', [
         "nome" => $dados->nome,
@@ -71,38 +53,34 @@ it('cria uma nova empresa', function () {
     ]);
 
     $response->assertCreated()
-        ->assertJsonFragment($empresaCriada->toArray());
+        ->assertJsonFragment(['nome' => $empresaCriada->nome])
+        ->assertJsonFragment(['cnpj' => $empresaCriada->cnpj]);
 });
 
 it('atualiza uma empresa existente', function () {
-    $dados = Empresa::factory()->make();
-    $empresaAtualizada = (new Empresa())->fill(
-        [
-            'nome' => 'Empresa Atualizada',
-            'endereco' => "novo endereço"
-        ]);
-    $empresaResource = new EmpresaResource($empresaAtualizada);
+    $dados = Empresa::factory()->create();
+    $empresa = Empresa::get()->first();
+    $empresa->nome = 'Empresa atualizado';
+    $empresa->endereco = 'endereço atualizado';
 
-    $this->mockService
-        ->shouldReceive('atualizar')
-        ->with(Mockery::type(UpdateEmpresaRequest::class), 5)
-        ->once()
-        ->andReturn($empresaResource);
-
-    $response = $this->put('/api/empresas/5', $dados->toArray());
+    $response = $this->put('/api/empresas/'.$empresa->id, [
+        'nome' => $empresa->nome,
+        'cnpj' => $empresa->cnpj,
+        'endereco' => $empresa->endereco
+    ]);
 
     $response->assertOk()
-        ->assertJsonFragment(['nome' => 'Empresa Atualizada','endereco' => "novo endereço"]);
+        ->assertJsonFragment(['nome' => $empresa->nome])
+        ->assertJsonFragment(['cnpj' => $empresa->cnpj])
+        ->assertJsonFragment(['endereco' => $empresa->endereco]);
 });
 
 it('remove uma empresa', function () {
-    $this->mockService
-        ->shouldReceive('remover')
-        ->with(5)
-        ->once()
-        ->andReturnTrue();
+    Empresa::factory()->create();
+    $empresa = Empresa::get()->first();
 
-    $response = $this->delete('/api/empresas/5');
+    $response = $this->delete('/api/empresas/'.$empresa->id);
 
     $response->assertNoContent();
+
 });
